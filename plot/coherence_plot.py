93254cfa7577be6d0dfd9ea606e18b3ff7065ff1
hyeechongg@gmail.com <219068559+hychong1811@users.noreply.github.com>
2026-04-19 13:30:41 +0800

    Adjust legend position

--- plot/coherence_plot.py
+++ plot/coherence_plot.py
@@ -287,7 +287,7 @@ def plot_complex_coherence_comparison(
     ax.set_ylabel(r"$\rho_{01}(t)$")
     if grid:
         ax.grid(True, alpha=0.3)
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
           fancybox=True, shadow=True, ncol=2)
     if title is not None:
         ax.set_title(title)
@@ -466,7 +466,7 @@ def plot_populations_and_coherence_comparison(
         ax.set_title(title)
     if grid:
         ax.grid(True, alpha=0.3)
-    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
           fancybox=True, shadow=True, ncol=2)
     fig.tight_layout()
     return fig, ax
